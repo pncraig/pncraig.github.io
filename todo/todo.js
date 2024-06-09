df4bf4f11d4ab7f3169f767889aafa817d1c18d8
Peter <76919512+pncraig@users.noreply.github.com>
2024-06-09 19:26:03 -0400

    Update todo.js

--- todo/todo.js
+++ todo/todo.js
@@ -6,11 +6,13 @@ function addItem() {
   // Access the text field element
   const textField = document.getElementById("text-field");
   // Get the text in the text field, which is stored in the "value" attribute
-  const item = textField.getAttribute("value");
+  const item = textField.value;
   // Create a new text node that will become a child of the new list element
   const textNode = document.createTextNode(item);
   // Add the text node as the child of the list element
   li.appendChild(textNode);
   // Append the list item to the ordered list
   ol.appendChild(li);
+  // Erase the text field
+  textField.value = "";
 }
