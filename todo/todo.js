b089ba9f3d1f4efb7922697494d8daa257f31c1f
Peter <76919512+pncraig@users.noreply.github.com>
2024-06-10 10:05:48 -0400

    Update todo.js

--- todo/todo.js
+++ todo/todo.js
@@ -3,6 +3,8 @@ const button = document.getElementById("button");
 // Add the addItem() function as the event handler for the button
 button.addEventListener("click", addItem);
 
+// Function that adds a new item to the todo list
+// when the button is pressed
 function addItem() {
   // Get the ordered list element
   const ol = document.getElementById("ordered-list");
@@ -14,13 +16,22 @@ function addItem() {
   const item = textField.value;
   // Create a new text node that will become a child of the new list element
   const textNode = document.createTextNode(item);
+
+  // Function that removes an item from the todo list
+  // when the checkbox is checked
+  function checkOffItem() {
+    li.remove();
+  }
+
   // Create a checkbox input element
   const check = document.createElement("input");
   check.setAttribute("type", "checkbox");
-  // Add the text node as the child of the list element
-  li.appendChild(textNode);
+  check.addEventListener("input", checkOffItem);
+  
   // Add the checkbox as a child of the list element
   li.appendChild(check);
+  // Add the text node as the child of the list element
+  li.appendChild(textNode);
   // Append the list item to the ordered list
   ol.appendChild(li);
   // Erase the text field
